fba29b0f76250648406013dd64353748b1a030a8
yusuf <shunan@gmail.com>
2018-12-03 14:11:34 +0500

    courses & institutes separated

---
+++ courses.js
@@ -0,0 +1,26 @@
+var institutes = require('request-promise');
+var courses = require('request-promise'); 
+institutes({
+  "method":"GET", 
+  "uri": "http://mqa.gov.mv/api/v1/institutes/local",
+  "json": true,
+  "headers": {
+    "User-Agent": "Data Mining"
+  }
+}).then(function(response){
+
+	for (i in response){
+		courses({
+	 		"method":"GET",
+  			"uri": "http://mqa.gov.mv/api/v1/institutes/"+response[i].id+"/courses",
+  			"json": true,
+			"headers": {
+    				"User-Agent": "Data Mining"
+  			}		
+		}).then(function(result){
+			const fs = require('fs');
+			fs.writeFileSync('courses.json', JSON.stringify(result)); 	
+		})
+	}
+});
+
